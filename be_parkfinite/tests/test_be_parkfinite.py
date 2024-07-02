b8dd972e55bc0bf510d372df919fd8436823c16e
Richard Harrison <r.harr.k@gmail.com>
2024-07-02 19:29:00 +0100

    starts to develop first genuine unit test in pytest

--- be_parkfinite/tests/test_be_parkfinite.py
+++ be_parkfinite/tests/test_be_parkfinite.py
@@ -3,7 +3,8 @@ from pydantic import BaseModel
 from typing import List
 from be_parkfinite.tests.test_data import test_data
 from fastapi.testclient import TestClient
-from be_parkfinite.utils.seed_database import seed_database
+from be_parkfinite.utils.databse_utils import seed_database
+from be_parkfinite.utils.test_utils import is_valid_date
 from be_parkfinite.main import app, get_db
 
 import models
@@ -20,17 +21,18 @@ test_engine = create_engine(
 TestSession = sessionmaker(
     autocommit=False, autoflush=False, bind=test_engine)
 testData = campsite = models.Campsite(
-            campsite_name="TEST XYZ",
-            campsite_longitude=-121.885,
-            campsite_latitude=37.338,
-            parking_cost=10.1
-        )
+    campsite_name="TEST XYZ",
+    campsite_longitude=-121.885,
+    campsite_latitude=37.338,
+    parking_cost=10.1
+)
 
 
 @pytest.fixture(scope='function')
 def test_db():
     seed_database(test_engine, TestSession, test_data)
     yield
+
 
 def override_get_db():
     try:
@@ -39,10 +41,10 @@ def override_get_db():
     finally:
         db.close()
 
+
 app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
-
 
 def test_read_main():
     # act
@@ -51,22 +53,22 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
 
-
-class ExpectedCampsite(BaseModel):
-    campsite_name: str
-    campsite_longitude: float
-    campsite_latitude: float
-    parking_cost: float
-    facilities_cost: float
-    description: str
-    campsite_id: int
-    category_id: int
-    photos: List
-    date_added: str
-    added_by: str
-
 def test_read_campsites(test_db):
     response = client.get("/campsites")
-    print(response.json(), "<<<<<<<<<<")
     assert response.status_code == 200
+
+    response_data = response.json()
+    assert len(response_data) == 1
+    for campsite in response_data:
+        assert isinstance(campsite['campsite_name'], str)
+        assert isinstance(campsite['description'], str)
+        assert isinstance(campsite['added_by'], str)
+        assert isinstance(campsite['campsite_longitude'], float)
+        assert isinstance(campsite['campsite_latitude'], float)
+        assert isinstance(campsite['campsite_id'], int)
+        assert isinstance(campsite['category_id'], int)
+        assert isinstance(campsite['photos'], list)
+        assert isinstance(campsite['date_added'], str)
+        assert is_valid_date(campsite['date_added'])
+
 
