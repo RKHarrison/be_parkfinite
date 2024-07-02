abb96e9cd7b53f4ac1f1177b69c3a594bdcc09de
Richard Harrison <r.harr.k@gmail.com>
2024-07-02 19:02:56 +0100

    corrects hadnling of seed data to be iterative for each data item

--- be_parkfinite/utils/seed_database.py
+++ be_parkfinite/utils/seed_database.py
@@ -1,12 +1,12 @@
 from database import Base
 
 def seed_database(engine, session, data):
-    print(data, "<<<DATA")
     db = session()
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
     try:
-        db.add(data)
+        for item in data.values():
+            db.add(item)
         db.commit()
     finally:
         db.close()
