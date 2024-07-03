c09b94f82aaf53ead800b9d6ab7857f7ce81c69f
Richard Harrison <r.harr.k@gmail.com>
2024-07-03 23:00:14 +0100

    adds to dev data

--- be_parkfinite/tests/test_data.py
+++ be_parkfinite/tests/test_data.py
@@ -4,20 +4,17 @@ from be_parkfinite.models.campsite_models import Campsite, CampsitePhoto, Campsi
 def get_test_data():
 
     test_campsite_photo_data = [CampsitePhoto(
-        campsite_photo_id=1,
         campsite_id=1,
         campsite_photo_url="https://example.com/photo1.jpg"
     )]
 
     test_campsite_contact_data = [CampsiteContact(
-        campsite_contact_id=1,
         campsite_id=1,
         campsite_contact_name="John Doe",
         campsite_contact_phone="123-456-7890"
     )]
 
     test_campsite_data = [Campsite(
-        campsite_id=1,
         campsite_name="McCampface",
         campsite_longitude=-121.885,
         campsite_latitude=37.338,
@@ -25,13 +22,11 @@ def get_test_data():
         facilities_cost=20.0,
         description="A beautiful campsite.",
         category_id=1,
-        # photos=[test_campsite_photo],
         date_added=datetime.now().isoformat(),
         added_by="Admin"
     )]
 
     test_campsite_category_data = [CampsiteCategory(
-        category_id=1,
         category_name="Mountain",
         category_image_url="https://example.com/category1.jpg"
     )]
