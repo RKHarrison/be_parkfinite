7b62002feee5f59d969dc3b97bbe48b4895ef865
Richard Harrison <r.harr.k@gmail.com>
2024-07-03 09:53:19 +0100

    completes extracting of seed_database function to database_utils.py function for reuse, completes util TDD, seperates concerns

---
+++ be_parkfinite/utils/database_utils.py
@@ -0,0 +1,7 @@
+from database import Base
+
+def seed_database(db, data):
+        for table_model in data.values():
+            for row in table_model:
+                db.add(row)
+        db.commit()
