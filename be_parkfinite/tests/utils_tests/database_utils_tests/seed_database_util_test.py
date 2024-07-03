f4aeb510f030282d355cb82cd19d5274038e8c0c
Richard Harrison <r.harr.k@gmail.com>
2024-07-03 15:20:30 +0100

    cleans up for pull request

--- be_parkfinite/tests/utils_tests/database_utils_tests/seed_database_util_test.py
+++ be_parkfinite/tests/utils_tests/database_utils_tests/seed_database_util_test.py
@@ -6,19 +6,13 @@ from be_parkfinite.utils.database_utils import seed_database
 from be_parkfinite.tests.utils_tests.database_utils_tests.seed_database_test_data import get_empty_seed_test_data, get_single_item_seed_test_data, get_single_model_seed_test_data, get_complex_seed_test_data
 
 
-
 @pytest.fixture(scope="function")
 def util_test_session():
-    # Create an SQLAlchemy engine that points to a SQLite database file.
-    util_test_engine = create_engine('sqlite:///./util_test.db', echo=True)
-    # Create all tables.
+    util_test_engine = create_engine('sqlite:///', echo=True)
     Base.metadata.create_all(bind=util_test_engine)
-    # Create a new session.
     Session = sessionmaker(bind=util_test_engine)
     util_test_session = Session()
-    # Provide the session to the test.
     yield util_test_session
-    # Close the session and drop all tables after the test is done.
     util_test_session.close()
     Base.metadata.drop_all(util_test_engine)
 
@@ -31,6 +25,7 @@ def test_seed_database_with_empty_data(util_test_session):
     # Assert
     all_items = util_test_session.query(Campsite).all()
     assert len(all_items) == 0
+
 
 def test_seed_database_with_single_item(util_test_session):
     # Arrange
@@ -47,6 +42,7 @@ def test_seed_database_with_single_item(util_test_session):
     assert seeded_campsite.campsite_longitude == -117.123
     assert seeded_campsite.campsite_latitude == 32.715
     assert seeded_campsite.description == "A lovely spot for sunset views"
+
 
 def test_seed_database_with_multiple_items_single_model(util_test_session):
     # Arrange
@@ -65,6 +61,7 @@ def test_seed_database_with_multiple_items_single_model(util_test_session):
     assert seeded_campsite_3.campsite_id == 3
     assert seeded_campsite_3.campsite_name == "Campy 3"
 
+
 def test_seed_database_with_complex_data(util_test_session):
     # Arrange
     complex_data = get_complex_seed_test_data()
@@ -79,4 +76,3 @@ def test_seed_database_with_complex_data(util_test_session):
     # Assert CampsiteCategories
     all_categories = util_test_session.query(CampsiteCategories).all()
     assert len(all_categories) == 2
-
