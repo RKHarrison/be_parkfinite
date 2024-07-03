b1ec7d83fee6dc72e731247cd4ad16153613d072
Richard Harrison <r.harr.k@gmail.com>
2024-07-03 19:45:59 +0100

    adds user and user_campsite(aka favourites) models and schemas

---
+++ be_parkfinite/schemas/user_schemas.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    username = str
+    user_password = str
+    user_firstname = str
+    user_lastname = str
+    user_email = str
+    xp = int
+    user_type = str
+    camera_permission = bool
+
+class CreateUser(UserBase):
+    pass    
+
+class User(UserBase):
+    pass
+
+
+
+class UserCampsiteBase(BaseModel):
+    user_id = int
+    campsite_id = int
+
+class CreateUserCampsite(UserCampsiteBase):
+    pass
+
+class UserCampsite(UserCampsiteBase):
+    user_campsite_id = int
