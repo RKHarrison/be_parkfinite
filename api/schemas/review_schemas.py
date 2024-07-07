28d7ff4db0d9dd2d545b0be466c71479e0c68771
Richard Harrison <r.harr.k@gmail.com>
2024-07-07 23:09:09 +0100

    corrects schema

--- api/schemas/review_schemas.py
+++ api/schemas/review_schemas.py
@@ -2,15 +2,15 @@ from pydantic import BaseModel
 
 class ReviewBase(BaseModel):
     rating: int
-    campsite_id: int
     username: str
     comment: str | None = None
 
-class CreateReview(ReviewBase):
+class ReviewCreateRequest(ReviewBase):
     pass
 
 class Review(ReviewBase):
     review_id: int
+    campsite_id: int
 
     class ConfigDict: 
         from_attributes = True
