605c8c5da29687ee6bff85dedde1762a3a959812
Richard Harrison <r.harr.k@gmail.com>
2024-07-08 01:04:25 +0100

    adds field validators to ReviewBase Pydantic schema model to rasie errors, picked up by Validation Error handler

--- api/errors/error_handling.py
+++ api/errors/error_handling.py
@@ -2,12 +2,23 @@ from fastapi import HTTPException, Request, status
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from fastapi.encoders import jsonable_encoder
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.exc import SQLAlchemyError, OperationalError
 from os import getenv
 
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     errors = exc.errors()
     body = exc.body
+    for error in errors:
+        if error['loc'] == ('body', 'comment') and error['msg'] == 'String should have at most 350 characters':
+            return JSONResponse(
+                status_code=422,
+                content={"detail": [{"msg": "String should have at most 350 characters", "loc": error['loc'], "type": error['type']}]},
+            )
+        if error['loc'] == ('body', 'rating') and error['msg'] == 'Rating should be between 1 and 5':
+            return JSONResponse(
+                status_code=422,
+                content={"detail": [{"msg": "Rating should be between 1 and 5", "loc": error['loc'], "type": error['type']}]},
+            )
     if getenv("ENV") == "development":
         print("Validation errors:", errors)
     return JSONResponse(
@@ -32,6 +43,13 @@ async def http_exception_handler(request: Request, exc: HTTPException):
     )
 
 async def sqlalchemy_exception_handler(request: Request, exc: SQLAlchemyError):
+    if isinstance(exc.orig, OperationalError):
+        if getenv("ENV") == "development":
+            print(f"Database error occurred: {str(exc)}")
+        return JSONResponse(
+            status_code=404,
+            content={"detail": "Resource not found!"},
+        )
     if getenv("ENV") == "development":
         print(f"Database error occurred: {str(exc)}")
     return JSONResponse(
--- api/schemas/review_schemas.py
+++ api/schemas/review_schemas.py
@@ -1,9 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
+from typing import Annotated
 
 class ReviewBase(BaseModel):
-    rating: int
+    rating: Annotated[int, Field(ge=1, le=5)]
     username: str
-    comment: str | None = None
+    comment: Annotated[str, Field(max_length=350)] | None = ""
+
+    @field_validator('rating')
+    def validate_rating(cls, rating):
+        if rating < 1 or rating > 5:
+            raise ValueError('Rating should be between 1 and 5')
+        return rating
+    
+    @field_validator('comment')
+    def validate_comment(cls, comment):
+        if comment is not None and len(comment) > 350:
+            raise ValueError('String should have at most 350 characters')
+        return comment
+
 
 class ReviewCreateRequest(ReviewBase):
     pass
