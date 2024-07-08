fd59fe69139c43b8f39eabc7d198b3fc74c8f078
Richard Harrison <r.harr.k@gmail.com>
2024-07-08 13:48:22 +0100

    creates functionality and basic 404 error handling for endpoint

--- api/main.py
+++ api/main.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from database.database import SessionLocal, engine, Base
 from api.crud.campsite_crud import create_campsite, read_campsites, read_campsite_by_id
 from api.crud.reviews_crud import create_review_by_campsite_id, read_reviews_by_campsite_id
-from api.crud.user_crud import read_users
+from api.crud.user_crud import read_users, read_user_by_username
 from api.schemas.campsite_schemas import CampsiteCreateRequest, Campsite, CampsiteDetailed
 from api.schemas.review_schemas import Review, ReviewCreateRequest
 from api.schemas.user_schemas import User
@@ -63,6 +63,9 @@ def get_reviews_by_campsite_id(campsite_id, db: Session = Depends(get_db)):
 def get_users(db: Session = Depends(get_db)):
     return read_users(db)
 
+@app.get("/users/{username}", response_model=User)
+def get_users(username, db: Session = Depends(get_db)):
+    return read_user_by_username(db, username)
 
 if __name__ == "__main__":
     PORT = int(getenv('PORT', 8000))
