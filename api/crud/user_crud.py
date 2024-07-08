3aa4d54fa21a4ece3519ef425df3373a048109d0
Richard Harrison <r.harr.k@gmail.com>
2024-07-08 19:50:53 +0100

    reorganises  for clarity

--- api/crud/user_crud.py
+++ api/crud/user_crud.py
@@ -3,6 +3,17 @@ from api.models.user_models import User, user_campsite_favourites
 from api.models.campsite_models import Campsite
 
 
+def read_users(db):
+    users = db.query(User).all()
+    return users
+
+def read_user_by_username(db, username):
+    user = db.get(User, username)
+    if not user:
+        raise HTTPException(status_code=404, detail="404 - User Not Found!")
+    return user
+
+
 
 def create_user_favourite_campsite(db, username, campsite_id):
     user = db.query(User).filter(User.username == username).first()
@@ -16,16 +27,6 @@ def create_user_favourite_campsite(db, username, campsite_id):
         user.favourites.append(campsite)
         db.commit()
     return
-
-def read_users(db):
-    users = db.query(User).all()
-    return users
-
-def read_user_by_username(db, username):
-    user = db.get(User, username)
-    if not user:
-        raise HTTPException(status_code=404, detail="404 - User Not Found!")
-    return user
 
 def read_user_campsite_favourites_by_username(db, username: str):
     user = db.query(User).filter(User.username == username).first()
