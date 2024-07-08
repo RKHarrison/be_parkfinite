0d7137bc933ed62427929a60e9dd31fbd5b0fa99
Richard Harrison <r.harr.k@gmail.com>
2024-07-08 01:05:23 +0100

    adds checks for existence of both campsite id and user id before adding review

--- api/crud/reviews_crud.py
+++ api/crud/reviews_crud.py
@@ -1,5 +1,7 @@
 from fastapi import HTTPException
 from api.models.review_models import Review
+from api.models.campsite_models import Campsite
+from api.models.user_models import User
 from api.schemas.review_schemas import ReviewCreateRequest
 
 
@@ -11,15 +13,21 @@ def read_reviews_by_campsite_id(db, id: int):
 
 
 def create_review_by_campsite_id(db, campsite_id: int, request: ReviewCreateRequest):
+    campsite = db.query(Campsite).filter(Campsite.campsite_id == campsite_id).first()
+    if not campsite:
+        raise HTTPException(status_code=404, detail="404 - Campsite Not Found!")
+    
+    user = db.query(User).filter(User.username == request.username).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="404 - User Not Found!")
+
     new_review = Review(
         campsite_id=campsite_id,
         rating=request.rating,
         comment=request.comment,
         username=request.username
     )
-    print(new_review)
     db.add(new_review)
     db.commit()
     db.refresh(new_review)
-    print(new_review)
     return new_review
